caa5b01ae03f6e2fbe1634ea1ac4805e5f16c154
daw2 <fatinebh23@gmail.com>
2024-02-06 12:58:03 +0100

    mas cambios

---
+++ EV2/Más Node.js/server5.js
@@ -0,0 +1,14 @@
+// Obtén el id del proceso
+const processId = process.pid;
+const processTitle = process.title;
+const nodeVersion = process.version;
+const operatingSystem = process.plataform;
+
+
+// Muestra la información por consola
+console.log('ID del proceso:', processId);
+console.log('Título del proceso:', processTitle);
+console.log('Versión de Node.js:', nodeVersion);
+console.log('Sistema Operativo:', operatingSystem);
+
+   
